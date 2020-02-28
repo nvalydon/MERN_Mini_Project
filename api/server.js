abbc7b7e8c59bcf98125c2fe105674f7ec6e6ae7
lukecottenham <luke.cottenham@hotmail.co.uk>
2020-02-28 11:36:54 +0000

    basic express set up

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const passport = require('./auth');
+const books = require('./routes/books');
+const users = require('./routes/users');
+const { session } = require('./config.json');
+const users = require('./routes/users')
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: session.secret, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(passport.authenticate('local'));
+app.use(express.json());
+
+app.use('/users', users);
+
+app.listen(9000);
